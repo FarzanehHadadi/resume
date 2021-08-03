59f996878432eca8d78f7f5b6f88482956edd333
farzanehHaddadi <farzhadadi@gmail.com>
2021-08-03 16:34:02 +0430

    summery added

---
+++ src/component/Summery.jsx
@@ -0,0 +1,38 @@
+const Summery = () => {
+  return (
+    <div className="summery-desc">
+      <h3 className="summery-title">Overiew</h3>
+      <p>
+        After graduating in 2012 with a Bachelor’s Degree in Information &#x26;
+        Technology Engineering from{" "}
+        <a href="https://shahroodut.ac.ir/fa/" target="_blank" rel="noreferrer">
+          Shahrood University of Technology
+        </a>{" "}
+        , I spent as a software developer 1.5 years working in{" "}
+        <a href="http://logicsims.ir/" target="_blank" rel="noreferrer">
+          Shabihbsazane Mantegh
+        </a>{" "}
+        company. I was a Visual C++ developer as a part of a team there.
+      </p>
+      <p>
+        Until 2019 I worked as a C# developer (for 5 years) in MAPCS team in{" "}
+        <a href="http://www.mapnaec.com/fa/" target="_blank" rel="noreferrer">
+          Meco
+        </a>{" "}
+        company.
+      </p>
+      <p>
+        In last two years I am working as a freelance front end web developer
+        with ReactJS.
+      </p>
+      <p>
+        Well-organised person, problem solver, independent employee with high
+        attention to detail. A high level of responsibility and teamwork, have a
+        strong interest in learning, Curious and motivated because programming
+        is my passion.
+      </p>
+    </div>
+  );
+};
+
+export default Summery;
